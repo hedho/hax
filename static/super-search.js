18ebbcbc1492e71e4f840f9894276f1878f7605e
monaco <arianitkukaj@gmail.com>
2022-01-15 02:52:44 +0100

    Update super-search.js

--- static/super-search.js
+++ static/super-search.js
@@ -63,7 +63,7 @@
     }
 
     var xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET","http://rss.app/feeds/5ycLBB7cYoCEwBOi.xml");
+    xmlhttp.open("GET","sitemap.xml");
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState != 4) return;
         if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
